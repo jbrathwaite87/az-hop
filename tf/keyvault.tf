3f81a958c76c2fa1f8294a383946dfd832dc8de7
jbrathwaite <59611009+jbrathwaite87@users.noreply.github.com>
2025-03-02 09:20:15 -0600

    Update keyvault.tf

--- tf/keyvault.tf
+++ tf/keyvault.tf
@@ -15,7 +15,7 @@ resource "azurerm_key_vault" "azhop" {
   enable_rbac_authorization = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days   = 7
-  purge_protection_enabled     = false
+  purge_protection_enabled     = true
   sku_name                     = "standard"
 
   network_acls {
