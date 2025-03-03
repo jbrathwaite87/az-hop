3e05bc2862839fc52c7250038d6c48fca0b0ec13
jbrathwaite <59611009+jbrathwaite87@users.noreply.github.com>
2025-03-03 05:09:58 -0600

    Update lustreclient.sh

--- packer/scripts/ubuntu/lustreclient.sh
+++ packer/scripts/ubuntu/lustreclient.sh
@@ -5,5 +5,5 @@ source /etc/lsb-release
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg
 
-apt-get update
-apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
+sudo apt-get update
+sudo apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
