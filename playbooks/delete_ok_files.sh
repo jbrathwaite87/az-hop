29f623d6a0aae7690b64e87370d89c8a1a434eeb
Jared Brathwaite <jared.brathwaite@alcority.com>
2025-03-08 06:28:59 -0600

    update

--- playbooks/delete_ok_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script deletes all files ending with .ok in the current directory and its subdirectories.
-
-echo "Deleting all *.ok files from $(pwd)..."
-find . -type f -name "*.ok" -delete
-echo "Deletion complete."
